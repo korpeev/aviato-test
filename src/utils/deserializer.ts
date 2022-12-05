62295311500a8f7ea2d2a0b06828d02688e2c94e
Aslan <korpeevaslan@gmail.com>
2022-12-05 19:57:08 +0500

    impl deserialize class

--- src/utils/deserializer.ts
+++ src/utils/deserializer.ts
@@ -1,11 +1,11 @@
 export class Deserializer {
-  static deserialize() {
-
-  }
-
-  private splitCamelCase(field: string) {
-    if (field.includes('_')) {
-      return field.split('_').map((char, index) => index % 2 === 0 ? char.charAt(0).toUpperCase() + char.slice(1) : char)
+  static deserialize(field: string) {
+    if (field.includes("_")) {
+      return field
+        .split("_")
+        .map((char, index) =>
+          index !== 0 ? char.charAt(0).toUpperCase() + char.slice(1) : char
+        ).join("");
     }
   }
 }
