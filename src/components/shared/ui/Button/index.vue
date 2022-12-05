79ad665a4c05edbcb6b91a0e58915ec7afb46809
Aslan <korpeevaslan@gmail.com>
2022-12-05 19:50:33 +0500

    add ui button

---
+++ src/components/shared/ui/Button/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="bg-accent text-white font-bold px-10 py-3 rounded-md" @click="emit('click')">Button</button>
+</template>
+
+<script lang="ts" setup>
+
+const emit = defineEmits<{(e: "click"): void}>()
+</script>
+<script lang="ts">
+export default {
+  name: "app-button"
+}
+</script>
