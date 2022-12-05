0b67dbead73dc5c5827a1483a1760f983b4d2c77
Aslan <korpeevaslan@gmail.com>
2022-12-05 19:47:44 +0500

    add tailwind css

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "accent": "#55BB06"
+      }
+    },
+  },
+  plugins: [],
+}
