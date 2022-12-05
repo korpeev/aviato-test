5fd8b18c778c20b413fd674faebc69c9e817a5e5
Aslan <korpeevaslan@gmail.com>
2022-12-05 19:48:57 +0500

    add mock db

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AppButton from "./components/shared/ui/Button/index.vue";</script>
+
+<template>
+  <div class="text-accent w-screen h-screen">
+    <span class="text-red-400">text</span>
+    <AppButton>clikc</AppButton>
+  </div>
+</template>
+
